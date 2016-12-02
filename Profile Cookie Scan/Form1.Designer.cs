06330165e505cc24bfaeb40da195cf0a96a8d21d
smthbh <smthbh@gmail.com>
2016-12-02 10:47:40 -0500

    Initial commit

--- Profile Cookie Scan/Form1.Designer.cs
+++ Profile Cookie Scan/Form1.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.CookieSearchWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button5 = new System.Windows.Forms.Button();
+            this.deleteWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(14, 134);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(383, 320);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Profile";
+            this.columnHeader1.Width = 103;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Cookies";
+            this.columnHeader2.Width = 71;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Path";
+            this.columnHeader3.Width = 205;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 93);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(383, 20);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 460);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 460);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select None";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 499);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(409, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(38, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "\\\\hqnas\\profiles$";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(93, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // CookieSearchWorker1
+            // 
+            this.CookieSearchWorker1.WorkerReportsProgress = true;
+            this.CookieSearchWorker1.WorkerSupportsCancellation = true;
+            this.CookieSearchWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.CookieSearchWorker1_DoWork);
+            this.CookieSearchWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.CookieSearchWorker1_ProgressChanged);
+            this.CookieSearchWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.CookieSearchWorker1_RunWorkerCompleted);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(252, 460);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 28);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Clean Selected Folders";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // deleteWorker
+            // 
+            this.deleteWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.deleteWorker_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 26);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Scans profiles in the directory for cookies in AppData\\Roaming\\Microsoft\\\r\nWindow" +
+    "s\\Cookies\\Low and allows you to select and clean up";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Profiles: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(409, 521);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Profile Cookie Scan";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker CookieSearchWorker1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button5;
+        private System.ComponentModel.BackgroundWorker deleteWorker;
+        public System.Windows.Forms.ListView listView1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Profile Cookie Scan/Form1.cs
+++ Profile Cookie Scan/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,207 @@ namespace Profile_Cookie_Scan
         public Form1()
         {
             InitializeComponent();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public int numdirs;
+
+        public void CookieSearchWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                try
+                {
+                    toolStripStatusLabel1.Text = ("Getting Directory: " + textBox1.Text);
+                    DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+                    numdirs = di.GetDirectories("*.v2").Length;
+                    int i = 0;
+                    if (listView1.InvokeRequired)
+                    {
+                        listView1.Invoke(new MethodInvoker(delegate
+                        {
+                            listView1.Items.Clear();
+                        }));
+                    }
+                    else
+                    {
+                        listView1.Items.Clear();
+                    }
+
+                    double percentdone = ((double)i / numdirs) * 100.0;
+
+                    foreach (var fold in di.GetDirectories("*.v2"))
+                    {
+                        if (CookieSearchWorker1.CancellationPending == true)
+                        {
+                            toolStripStatusLabel1.Text = "Cancelled!";
+                            break;
+                        }
+
+                        int numcookies = 0;
+                        i++;
+                        percentdone = ((double)i / numdirs) * 100.0;
+
+                        toolStripStatusLabel1.Text = ("Scanning " + i + ": " + fold.Name);
+
+
+                        if (Directory.Exists(fold.FullName + @"\AppData\Roaming\Microsoft\Windows\Cookies\Low"))
+                        {
+                            DirectoryInfo cookiedir = new DirectoryInfo((fold.FullName + @"\AppData\Roaming\Microsoft\Windows\Cookies\Low"));
+                            numcookies = cookiedir.GetFiles("*.txt").Length;
+
+                            string[] arr = new string[4];
+
+                            ListViewItem itm;
+
+                            arr[0] = fold.Name;
+                            arr[1] = numcookies.ToString();
+                            arr[2] = cookiedir.FullName;
+
+                            itm = new ListViewItem(arr);
+
+                            if (listView1.InvokeRequired)
+                            {
+                                listView1.Invoke(new MethodInvoker(delegate
+                                {
+                                    listView1.Items.Add(itm);
+                                }));
+                            }
+                            else
+                            {
+                                listView1.Items.Add(itm);
+                            }
+                        }
+
+                        CookieSearchWorker1.ReportProgress((int)percentdone, fold.Name);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(("Error occurred: " + ex.Message));
+                    toolStripStatusLabel1.Text = ("Error: " + ex.Message);
+                    button3.Enabled = true;
+                    button4.Enabled = false;
+                    throw;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show(("Directory " + textBox1.Text + " not found"));
+            }
+            button3.Enabled = true;
+            button4.Enabled = false;
+        }
+
+        public void CookieSearchWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void CookieSearchWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = ("Done");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button4.Enabled = true;
+            CookieSearchWorker1.RunWorkerAsync();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CookieSearchWorker1.CancelAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem listItem in listView1.Items)
+            {
+                listItem.Checked = true;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem listItem in listView1.Items)
+            {
+                listItem.Checked = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            progressBar1.Value = 1;
+            toolStripStatusLabel1.Text = ("Cleaning");
+            deleteWorker.RunWorkerAsync();
+        }
+
+        private void deleteWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                int numitems = listView1.Items.Count;
+                int i = 0;
+
+                foreach (ListViewItem listItem in listView1.Items)
+                {
+                    i++;
+                    progressBar1.Value = (int)(((double)i / numitems) * 100.0);
+
+                    if (listItem.Checked == true)
+                    {
+                        toolStripStatusLabel1.Text = ("Cleaning: " + listItem.Text);
+
+                        DirectoryInfo di = new DirectoryInfo(listItem.SubItems[2].Text);
+
+                        foreach (FileInfo file in di.GetFiles())
+                        {
+                            if (file.Exists)
+                            {
+                                //MessageBox.Show("Would delete: " + file.Name);
+                                try
+                                {
+                                    file.Delete();
+                                }
+                                finally { }
+                                
+                            }
+
+                        }
+                    }
+                }
+                toolStripStatusLabel1.Text = ("Done");
+                button5.Enabled = true;
+                MessageBox.Show("Profiles cleaned");
+
+            }
+            catch (Exception ex)
+            {
+                button5.Enabled = true;
+                MessageBox.Show(("Error occurred: " + ex.Message));
+                toolStripStatusLabel1.Text = ("Error: " + ex.Message);
+                throw;
+            }
+
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            //listView1.Sorting = SortOrder.Ascending;
         }
     }
 }
